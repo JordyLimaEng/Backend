0861e6e1936c75a31a8faa937b6ff10954063c04
Jordy Lima <jordylima.dev@gmail.com>
2023-08-16 14:18:25 -0300

    Otimizing tests with initial data created on startup

--- src/main/java/com/jordy/backend/BackendApplication.java
+++ src/main/java/com/jordy/backend/BackendApplication.java
@@ -1,13 +1,31 @@
 package com.jordy.backend;
 
+import com.jordy.backend.domain.Category;
+import com.jordy.backend.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		categoryRepository.deleteAll();
+
+		Category cat1 = new Category(null, "Computers");
+		Category cat2 = new Category(null, "Office");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
